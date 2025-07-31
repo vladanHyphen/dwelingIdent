de9c0e0f0db9e94ce8c89d253c6580aa8d159ef7
vladanHyphen <146004648+vladanHyphen@users.noreply.github.com>
2025-07-31 20:29:52 +0200

    Update app.py

--- app.py
+++ app.py
@@ -168,3 +168,4 @@ if st.button("Download Map and Detect Buildings"):
         st.error(f"An unexpected error occurred: {e}")
         import traceback
         st.text(traceback.format_exc())
+        
